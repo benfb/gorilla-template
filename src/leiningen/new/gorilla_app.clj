799b9ef63130eb86d860369a12bb92594a98657e
Ben Bailey <bennettbailey@gmail.com>
2016-10-17 19:32:54 -0400

    initial commit

---
+++ src/leiningen/new/gorilla_app.clj
@@ -0,0 +1,27 @@
+(ns leiningen.new.gorilla-app
+  (:require [leiningen.new.templates :refer [renderer year date project-name sanitize-ns multi-segment name-to-path ->files]]
+            [leiningen.core.main :as main]))
+
+(def render (renderer "gorilla-app"))
+
+(defn gorilla-app
+  "FIXME: write documentation"
+  [name]
+  (let [main-ns (multi-segment (sanitize-ns name))
+        data {:raw-name name
+              :name (project-name name)
+              :namespace main-ns
+              :nested-dirs (name-to-path main-ns)
+              :year (year)
+              :date (date)}]
+    (main/info "Generating fresh 'lein new' gorilla-app project.")
+    (->files data
+             ["project.clj" (render "project.clj" data)]
+             ["README.md" (render "README.md" data)]
+             ["doc/intro.md" (render "intro.md" data)]
+             [".gitignore" (render "gitignore" data)]
+             [".hgignore" (render "hgignore" data)]
+             ["src/{{nested-dirs}}.clj" (render "core.clj" data)]
+             ["test/{{nested-dirs}}_test.clj" (render "test.clj" data)]
+             ["LICENSE" (render "LICENSE" data)]
+             ["CHANGELOG.md" (render "CHANGELOG.md" data)])))
