2f849abd78a40adc22e524c0bd2c33736225e09a
Ben Bailey <bennettbailey@gmail.com>
2017-01-01 14:06:40 -0600

    revert to official gorilla dependency

--- resources/leiningen/new/gorilla_app/project.clj
+++ resources/leiningen/new/gorilla_app/project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
-  :plugins [[org.clojars.benfb/lein-gorilla "0.4.0"]]
+  :plugins [[lein-gorilla "0.4.0"]]
   :main ^:skip-aot {{namespace}}
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
