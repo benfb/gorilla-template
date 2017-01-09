76db3b3ca5f4d6992f62e7c2c20b42941430b43c
Ben Bailey <bennettbailey@gmail.com>
2017-01-08 22:35:22 -0600

    version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject gorilla-app/lein-template "0.5.1"
+(defproject gorilla-app/lein-template "0.5.2"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
