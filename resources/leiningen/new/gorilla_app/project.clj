aa026629aae500ce4ce04fc7a3a151c772b73bb0
Ben Bailey <bennettbailey@gmail.com>
2018-11-23 21:31:45 -0600

    new gorilla version

--- resources/leiningen/new/gorilla_app/project.clj
+++ resources/leiningen/new/gorilla_app/project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]]
-  :plugins [[org.clojars.benfb/lein-gorilla "0.5.1"]]
+  :plugins [[org.clojars.benfb/lein-gorilla "0.5.2"]]
   :main ^:skip-aot {{namespace}}
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
